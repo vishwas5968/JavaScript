2871a220c2a11ee84a27cc6a65d051d532c5390b
vishwas5968 <vishwasr968@gmail.com>
2024-03-25 09:18:56 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+let a = 1
+let b = true
+let c = "Hello"
+let d = 'h'
+let e = {
+    car:"bmw"
+}
+let f
+let g = null
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof typeof e)
+console.log(typeof f)
+console.log(typeof typeof null)
