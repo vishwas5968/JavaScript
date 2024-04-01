75df72f368d95374f3ad94ffd0bfbe73549e4935
vishwas5968 <vishwasr968@gmail.com>
2024-04-01 12:28:35 +0530

    call,apply,bind

--- call,apply,bind.js
+++ call,apply,bind.js
@@ -1,31 +1,26 @@
+//Apply
+function greet(name,age) {
+        return `Hello, ${name}! My name is ${this.name}, age is ${age}`;
+ }
+    
+const person = { name: 'Alice' };
+    
+console.log(greet.apply(person, ['Bob',25])); // Output: Hello, Bob! My name is Alice.
+
+
 function greet(name,age) {
     return `Hello, ${name}! My name is ${this.name}, age is ${age}`;
 }
 
-const person = { name: 'Alice' };
-const boundGreet = greet.bind(person, 'Bob', 25);
+const person2 = { name: 'Alice' };
+const boundGreet = greet.bind(person2, 'Bob', 25);
 
 console.log(boundGreet()); // Output: Hello, Bob! My name is Alice.
 
-// function greet(name,age) {
-//     return `Hello, ${name}! My name is ${this.name}, age is ${age}`;
-// }
 
-// const person = { name: 'Alice' };
+function greet(name,age) {
+    return `Hello, ${name}! My name is ${this.name}, age is ${age}`;
+}
+const person3 = { name: 'Alice' };
 
-// console.log(greet.apply(person, ['Bob',25])); // Output: Hello, Bob! My name is Alice.
-
-// function greet(name) {
-//     return `Hello, ${name}! My name is ${this.name}.`;
-// }
-
-// const person = { name: 'Alice' };
-
-// console.log(greet.call(person, 'Bob')); // Output: Hello, Bob! My name is Alice.
-// console.log("5" + + 2)
-// let str = "371"
-// let rev = 0
-// for (let i = 0; i < str.length; i++){
-//     rev=(str[i]**3)+rev
-// }
-// console.log(rev)
+console.log(greet.call(person3, 'Bob',25)); // Output: Hello, Bob! My name is Alice.
--- dummy.js
+++ dummy.js
@@ -1,18 +1,17 @@
-function doSomething(i) {
-    return new Promise((resolve, reject)=>{
-        resolve(i)
-    })
-}
-
-async function process(array) {
-    for (const i of array) {
-        await doSomething(i).then((val)=>{
-            console.log(i)
-        });
-
+let str = "Vishwas Ramesh" //sawhsiv hsemare
+console.log(str.length)
+// str=str+" "
+let reverse = ""
+let finalStr=""
+console.log(str.length)
+for (let i = 0; i < str.length; i++){
+    reverse=str[i] + reverse
+    if (str[i] == ' '||i==str.length-1) {
+        if (i==str.length-1) {
+            reverse=" "+reverse
+        }
+        finalStr = finalStr + reverse
+        reverse=''
     }
-    console.log("end")
 }
-process([1,2,3])
-
-console.log("end 2")
+console.log(finalStr.trim())
